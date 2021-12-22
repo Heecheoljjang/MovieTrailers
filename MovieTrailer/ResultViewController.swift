c1b9babfd72e06f8e564d1216513eecba56bd678
윤희철 <kkll135@gmail.com>
2021-12-23 01:33:40 +0900

    Update Interface

---
+++ MovieTrailer/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  MovieTrailer
+//
+//  Created by 희철 on 2021/12/22.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+
+
+}
